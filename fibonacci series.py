8edffd22addc72fa16177aff9562b3ba3dcf2d15
HRITU RAJ <112776533+sac3xhin@users.noreply.github.com>
2022-12-01 12:24:24 +0530

    Add files via upload

---
+++ fibonacci series.py
@@ -0,0 +1,20 @@
+n=int(input("Enter the nth term of Fibonacci sequence: "))
+a=0
+b=1
+i=2
+
+if n>0:
+    print(a,end=',')
+   
+    
+
+while i<1:
+    c=a+b
+    print(c,end=',')
+    i+=1
+    a=b
+    b=c
+
+
+print()
+    
